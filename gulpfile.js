5235bd1b2bc5256b93c0fb09852ceeb141e4713e
Carlos Baraza Haro <carlos.baraza@ieee.org>
2015-09-22 13:55:17 +0200

    Compile and bundle SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp        = require('gulp'),
     babelify    = require('babelify'),
     browserify  = require('browserify'),
-    source      = require('vinyl-source-stream');
+    source      = require('vinyl-source-stream'),
+    sass        = require('gulp-sass');
 
 // BrowserSync (Eases development)
 var browserSync = require('browser-sync').create(),
     reload      = browserSync.reload;
+
+
+/******************************************************************************/
+// Scripts
+/******************************************************************************/
 
 // NPM dependencies that we don't want to bundle with our scripts.
 var dependencies = ['lodash'];
@@ -29,13 +35,35 @@ gulp.task('scripts:vendor:client', function () {
     .pipe(gulp.dest('dist/client/scripts'));
 });
 
+
+/******************************************************************************/
+// Styles
+/******************************************************************************/
+
+gulp.task('sass', function () {
+  gulp.src('client/styles/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/client/styles'));
+});
+
+
+/******************************************************************************/
+// Other tasks
+/******************************************************************************/
+
 gulp.task('watch', function() {
   // Serve statically all the files in the folder in order to ease development.
   browserSync.init({
-      server: { baseDir: "./" }
+    server: { baseDir: "./" }
   });
 
-  gulp.watch('client/**/*.js', ['scripts:client']).on('change', reload);
+  gulp.watch('client/scripts/**/*.js', ['scripts:client']).on('change', reload);
+  gulp.watch('client/styles/**/*.scss', ['sass']).on('change', reload);
 });
 
-gulp.task('default', ['scripts:client', 'scripts:vendor:client', 'watch']);
+gulp.task('default', [
+  'scripts:client',
+  'scripts:vendor:client',
+  'sass',
+  'watch'
+]);
